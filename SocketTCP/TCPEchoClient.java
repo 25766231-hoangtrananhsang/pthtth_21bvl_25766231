dda995802400a1d850028f15b2ee8877013b56a8
25766231-hoangtrananhsang <anhsanghoangtran85@gmail.com>
2026-09-15 21:00:45 +0700

    Update TCPEchoClient.java

--- SocketTCP/TCPEchoClient.java
+++ SocketTCP/TCPEchoClient.java
@@ -1,1 +1,42 @@
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.Socket;
+
+public class TCPEchoclient {
+    public static final String serverIP = "127.0.0.1";
+    public static final int serverPort = 1024;
+
+    public static void main(String[] args) {
+        Socket s = null;
+        try {
+            s = new Socket(serverIP, serverPort);
+            System.out.println("Client da duoc tao");
+
+            InputStream is = s.getInputStream();
+            OutputStream os = s.getOutputStream();
+
+            for (int i = '0'; i <= '9'; i++) {
+                os.write(i);
+                int ch = is.read();
+                System.out.println((char) ch);
+                Thread.sleep(2000);
+            }
+
+        } catch (IOException ie) {
+            System.out.println("Error: Can NOT create socket");
+            ie.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (s != null) {
+                    s.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
